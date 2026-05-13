38d593af748ad23e4d848cc51b3b7c71ddc439e8
B3N64 <b3n64@protonmail.com>
2026-05-13 19:59:16 +0200

    signals überarbeitet.

--- src/signals.rs
+++ src/signals.rs
@@ -1,55 +1,64 @@
 use std::f64::consts::PI;
 
 pub trait Signal {
-    fn value_at(param: &SignalParams, t: f64) -> f64;
+    fn value_at(&self, t: f64) -> f64;
 }
+
 pub struct SignalParams {
     pub amplitude: f64,
     pub frequency: f64,
     pub phase: f64,
 }
 
-pub struct Sinus;
+pub struct Sinus {
+    param: SignalParams,
+}
 
 impl Signal for Sinus {
-    fn value_at(param: &SignalParams, t: f64) -> f64 {
-        param.amplitude * (2.0 * PI * param.frequency * t + param.phase).sin()
+    fn value_at(&self, t: f64) -> f64 {
+        self.param.amplitude * (2.0 * PI * self.param.frequency * t + self.param.phase).sin()
     }
 }
 
-pub struct Cosinus;
+pub struct Cosinus {
+    param: SignalParams,
+}
 
 impl Signal for Cosinus {
-    fn value_at(param: &SignalParams, t: f64) -> f64 {
-        param.amplitude * (2.0 * PI * param.frequency * t + param.phase).cos()
+    fn value_at(&self, t: f64) -> f64 {
+        self.param.amplitude * (2.0 * PI * self.param.frequency * t + self.param.phase).cos()
     }
 }
 
-pub struct Rectangular;
+pub struct Rectangular {
+    param: SignalParams,
+}
 
 impl Signal for Rectangular {
-    fn value_at(param: &SignalParams, t: f64) -> f64 {
-        if (2.0 * PI * param.frequency * t + param.phase).sin() >= 0.0 {
-            param.amplitude
+    fn value_at(&self, t: f64) -> f64 {
+        if (2.0 * PI * self.param.frequency * t + self.param.phase).sin() >= 0.0 {
+            self.param.amplitude
         } else {
-            -param.amplitude
+            -self.param.amplitude
         }
     }
 }
 
-pub struct Triangle;
+pub struct Triangle {
+    param: SignalParams,
+}
 
 impl Signal for Triangle {
-    fn value_at(param: &SignalParams, t: f64) -> f64 {
-        let period = 1.0 / param.frequency;
-        let x = ((t + param.phase) / period) % 1.0;
+    fn value_at(&self, t: f64) -> f64 {
+        let period = 1.0 / self.param.frequency;
+        let x = ((t + self.param.phase / (2.0 * PI)) / period) % 1.0;
 
         if x < 0.25 {
-            4.0 * param.amplitude * x
+            4.0 * self.param.amplitude * x
         } else if x < 0.75 {
-            2.0 * param.amplitude - 4.0 * param.amplitude * x
+            2.0 * self.param.amplitude - 4.0 * self.param.amplitude * x
         } else {
-            -4.0 * param.amplitude + 4.0 * param.amplitude * x
+            -4.0 * self.param.amplitude + 4.0 * self.param.amplitude * x
         }
     }
 }
