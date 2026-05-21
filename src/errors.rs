26440fab7f04cbbc6eee0152ebaf6f90dea60d4b
B3N64 <91955311+B3N64@users.noreply.github.com>
2026-05-21 14:26:18 +0200

    CircuitConstructError hinzugefügt

--- src/errors.rs
+++ src/errors.rs
@@ -5,12 +5,14 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Debug)]
 pub(crate) enum Error {
     NegativeFrequency,
+    CircuitConstructError(String),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &self {
             Error::NegativeFrequency => write!(f, "Frequency cannot be negative"),
+            Error::CircuitConstructError(var) => write!(f, "Missing required variable: {}", var),
         }
     }
 }
