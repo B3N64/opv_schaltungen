c3860bed685effdbebef7fc971a665ee4ecaa12c
B3N64 <91955311+B3N64@users.noreply.github.com>
2026-06-02 13:04:31 +0200

    tiefpass logik

--- src/circuits.rs
+++ src/circuits.rs
@@ -1,5 +1,4 @@
 use crate::errors::{Error, Result};
-use crate::signals::Signal;
 use std::f64::consts::PI;
 use std::ops::Add;
 
@@ -7,17 +6,19 @@ enum CircuitType {
     Integrator,
     Differentiator,
     Tiefpass,
+    // TODO: mit Hochpass ergänzen
 }
 
 impl CircuitType {
-    fn id(&self) -> &'static str {
-        match self {
-            CircuitType::Integrator => "Integrator",
-            CircuitType::Differentiator => "Differentiator",
-            CircuitType::Tiefpass => "Tiefpass",
-        }
+    fn all() -> &'static [CircuitType] {
+        &[
+            CircuitType::Integrator,
+            CircuitType::Differentiator,
+            CircuitType::Tiefpass,
+        ]
     }
-    fn name(&self, name: &str) -> Option<CircuitType> {
+
+    fn from_id(&self, name: &str) -> Option<CircuitType> {
         match name.to_lowercase().as_str() {
             "integrator" => Some(CircuitType::Integrator),
             "differentiator" => Some(CircuitType::Differentiator),
@@ -25,6 +26,23 @@ impl CircuitType {
             _ => None,
         }
     }
+
+    fn id(&self) -> &'static str {
+        match self {
+            CircuitType::Integrator => "integrator",
+            CircuitType::Differentiator => "differentiator",
+            CircuitType::Tiefpass => "tiefpass",
+        }
+    }
+
+    fn name(&self) -> &'static str {
+        match self {
+            CircuitType::Integrator => "Integrator",
+            CircuitType::Differentiator => "Differentiator",
+            CircuitType::Tiefpass => "Tiefpass",
+        }
+    }
+
     fn image(&self) -> &'static str {
         match self {
             CircuitType::Integrator => "/images/integrator.png",
@@ -32,6 +50,7 @@ impl CircuitType {
             CircuitType::Tiefpass => "/images/tiefpass.png",
         }
     }
+
     fn variables(&self) -> &'static [&'static str] {
         match self {
             CircuitType::Integrator => &["R", "C"],
@@ -71,29 +90,13 @@ pub trait Circuit {
     fn cutoff_frequency(&self) -> f64;
     fn amplitude_at(&self, frequenz: f64) -> f64;
     fn phase_at(&self, frequenz: f64) -> f64;
-    fn simulate(&mut self, signal: &dyn Signal, duration: f64, step: f64) -> Vec<(f64, f64)> {
-        let num_steps = (duration / step) as usize;
-        let mut results = Vec::with_capacity(num_steps);
-
-        for i in 0..num_steps {
-            let t = i as f64 * step;
-            let ue = signal.value_at(t);
-            let ua = self.output_voltage(ue, step);
-            results.push((t, ua));
-        }
-
-        let test = results[1];
-        results[0] = test;
-
-        results
-    }
 }
 
 pub struct Tiefpass {
     r1: f64,
     ck: f64,
     rk: f64,
-    last_ue: f64,
+    last_ua: f64,
 }
 
 impl Tiefpass {
@@ -102,35 +105,37 @@ impl Tiefpass {
             r1,
             ck,
             rk,
-            last_ue: 0.0,
+            last_ua: 0.0,
         }
     }
 }
 
 impl Circuit for Tiefpass {
     fn output_voltage(&mut self, ue: f64, dt: f64) -> f64 {
-        // U_out(t) = (1/(R1*CK)) * ∫ (U_in(t) - U_out(t)) dt
-        // ua[n] = ua[n−1] + (Δt/(R1*CK)) * (ue[n] - ua[n−1])
-        let ua = self.last_ue + (dt / (self.r1 * self.ck)) * (ue - self.last_ue);
-        self.last_ue = ua;
+        let derivative = -self.last_ua / (self.rk * self.ck) - ue / (self.r1 * self.ck);
+        let ua = self.last_ua + derivative * dt;
+
+        self.last_ua = ua;
         ua
     }
 
     fn cutoff_frequency(&self) -> f64 {
-        // |H(jw)| = 1/√(1+(wR1CK)^2) = 1/√2  =>  wR1CK = 1  =>  f = 1/(2πR1CK)
-        1.0 / (2.0 * PI * self.r1 * self.ck)
+        1.0 / (2.0 * PI * self.rk * self.ck)
     }
 
     fn amplitude_at(&self, frequenz: f64) -> f64 {
-        // |H(jw)| = 1/√(1+(wR1CK)^2)
-        let w = 2.0 * PI * frequenz;
-        1.0 / (1.0 + (w * self.r1 * self.ck).powi(2)).sqrt()
+        let f = frequenz.max(f64::MIN_POSITIVE);
+        let omega = 2.0 * PI * f;
+        let dc_gain = self.rk / self.r1;
+
+        dc_gain / (1.0 + (omega * self.rk * self.ck).powi(2)).sqrt()
     }
 
     fn phase_at(&self, frequenz: f64) -> f64 {
-        // H(jw) = 1/(1+jwR1CK)  => Phase = -arctan(wR1CK)
-        let w = 2.0 * PI * frequenz;
-        -(w * self.r1 * self.ck).atan()
+        let f = frequenz.max(f64::MIN_POSITIVE);
+        let omega = 2.0 * PI * f;
+
+        PI - (omega * self.rk * self.ck).atan()
     }
 }
 
@@ -148,26 +153,21 @@ impl Integrator {
 
 impl Circuit for Integrator {
     fn output_voltage(&mut self, ue: f64, dt: f64) -> f64 {
-        // U_out(t) = -1/(R*C) * ∫ U_in(t) dt
-        // ua[n] = ua[n−1] − 1/(R*C) * Δt * ue[n]
         let ua = self.last_ua - 1.0 / (self.r * self.c) * dt * ue;
         self.last_ua = ua;
         ua
     }
 
     fn cutoff_frequency(&self) -> f64 {
-        // |H(jw)| = 1/(wRC) = 1  =>  w = 1/(RC)  =>  f = 1/(2πRC)
         1.0 / (2.0 * PI * self.r * self.c)
     }
 
     fn amplitude_at(&self, frequenz: f64) -> f64 {
-        // |H(jw)| = 1/(ωRC)
         let f = frequenz.max(f64::MIN_POSITIVE);
         1.0 / (2.0 * PI * f * self.r * self.c)
     }
 
     fn phase_at(&self, _frequenz: f64) -> f64 {
-        // H(jw) = -1/(jωRC) = +j/(ωRC)  => Phase = +90°
         PI / 2.0
     }
 }
@@ -186,25 +186,20 @@ impl Differentiator {
 
 impl Circuit for Differentiator {
     fn output_voltage(&mut self, ue: f64, dt: f64) -> f64 {
-        // U_out(t) = -R*C * dU_in(t)/dt
-        // ua[n] = -R*C * (ue[n] - ue[n−1]) / Δt
         let ua = -self.r * self.c * (ue - self.last_ue) / dt;
         self.last_ue = ue;
         ua
     }
 
     fn cutoff_frequency(&self) -> f64 {
-        // |H(jw)| = ωRC = 1  =>  f = 1/(2πRC)
         1.0 / (2.0 * PI * self.r * self.c)
     }
 
     fn amplitude_at(&self, frequenz: f64) -> f64 {
-        // |H(jw)| = ωRC
         2.0 * PI * frequenz * self.r * self.c
     }
 
     fn phase_at(&self, _frequenz: f64) -> f64 {
-        // H(jw) = -jωRC  => Phase = -90°
         -PI / 2.0
     }
 }
